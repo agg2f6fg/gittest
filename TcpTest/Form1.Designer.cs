50e5a045872d169e25082dc087d76d5e1a0827db
Max <agg2f6fg@gmail.com>
2022-03-24 22:44:22 +0800

    新增 gitTest
    Signed-off-by: Max <agg2f6fg@gmail.com>

---
+++ TcpTest/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace TcpTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnTransfer = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textIP = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textPort = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(54, 38);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnTransfer
+            // 
+            this.btnTransfer.Location = new System.Drawing.Point(218, 38);
+            this.btnTransfer.Name = "btnTransfer";
+            this.btnTransfer.Size = new System.Drawing.Size(75, 23);
+            this.btnTransfer.TabIndex = 1;
+            this.btnTransfer.Text = "Transfer";
+            this.btnTransfer.UseVisualStyleBackColor = true;
+            this.btnTransfer.Click += new System.EventHandler(this.btnTransfer_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(374, 38);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(86, 23);
+            this.btnDisconnect.TabIndex = 2;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(62, 151);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(398, 158);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textIP
+            // 
+            this.textIP.Location = new System.Drawing.Point(76, 99);
+            this.textIP.Name = "textIP";
+            this.textIP.Size = new System.Drawing.Size(133, 25);
+            this.textIP.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "IP : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(261, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Port :";
+            // 
+            // textPort
+            // 
+            this.textPort.Location = new System.Drawing.Point(306, 99);
+            this.textPort.Name = "textPort";
+            this.textPort.Size = new System.Drawing.Size(133, 25);
+            this.textPort.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 506);
+            this.Controls.Add(this.textPort);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textIP);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.btnTransfer);
+            this.Controls.Add(this.btnConnect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnTransfer;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textIP;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textPort;
+    }
+}
+
---
+++ TcpTest/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+
+namespace TcpTest
+{
+    public partial class Form1 : Form
+    {
+        private event EventHandler OnRecieveData;
+
+        private TcpClient m_Client;
+        private NetworkStream stream;
+        private string m_IP;
+        private string m_Port;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            m_IP = textIP.Text;
+            m_Port = textPort.Text;
+            m_Client = new TcpClient( m_IP, Convert.ToInt16(m_Port) );
+        }
+
+        private void btnTransfer_Click(object sender, EventArgs e)
+        {
+            byte[] data = System.Text.Encoding.ASCII.GetBytes(textBox1.Text);
+            try
+            {
+                stream = m_Client.GetStream();
+                stream.Write(data, 0, data.Length);
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("未連線");
+            }
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            byte[] data = System.Text.Encoding.Unicode.GetBytes("disconnect");
+
+            stream = m_Client.GetStream();
+            stream.Write(data, 0, data.Length);
+            stream.Close();
+            m_Client.Close();  
+        }
+        
+        void client_OnRecieveData( object Sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
